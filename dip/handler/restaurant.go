08aa718745c022e07d7ba6135e7903874db8c2b3
aidostt <leket1999@gmail.com>
2024-04-26 21:22:25 +0500

    refactored restaurant delivery layer from http to grpc

--- dip/handler/restaurant.go
+++ dip/handler/restaurant.go
@@ -1,91 +1,136 @@
 package handler
 
 import (
+	"context"
+	"dip/internal/logger"
 	"dip/models"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"errors"
+	proto_restaurant "github.com/aidostt/protos/gen/go/reservista/restaurant"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func (h *Handler) GetAllRestaurants(c *gin.Context) {
-	res, err := h.service.Restaurants.GetAll(c.Request.Context())
+func (h *Handler) GetAllRestaurants(ctx context.Context, input *proto_restaurant.Empty) (*proto_restaurant.RestaurantListResponse, error) {
+	restaurants, err := h.service.Restaurants.GetAll(ctx)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"bad reques": err.Error()})
-		c.Abort()
-		return
+		logger.Error(err)
+		switch {
+		default:
+			return nil, status.Error(codes.Internal, "internal error")
+		}
 	}
-	c.JSON(http.StatusOK, res)
+	restaurantResponse := make([]*proto_restaurant.RestaurantObject, len(restaurants))
+	for index, restaurant := range restaurants {
+		restaurantResponse[index] = &proto_restaurant.RestaurantObject{
+			Id:      restaurant.ID.String(),
+			Name:    restaurant.Name,
+			Address: restaurant.Address,
+			Contact: restaurant.Contact,
+		}
+	}
+	return &proto_restaurant.RestaurantListResponse{
+		Restaurants: restaurantResponse,
+	}, nil
 }
 
-func (h *Handler) GetRestaurant(c *gin.Context) {
-	var getRestInp models.GetByIdInputSql
-	if err := c.BindJSON(&getRestInp); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		c.Abort()
-		return
+func (h *Handler) GetRestaurant(ctx context.Context, input *proto_restaurant.IDRequest) (*proto_restaurant.RestaurantObject, error) {
+	if input.Id == "" {
+		return nil, status.Error(codes.InvalidArgument, "id is required")
 	}
 
-	res, err := h.service.Restaurants.GetById(c.Request.Context(), getRestInp.ID)
+	res, err := h.service.Restaurants.GetById(ctx, input.GetId())
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"bad reques": err.Error()})
-		c.Abort()
-		return
+		logger.Error(err)
+		switch {
+		case errors.Is(err, errors.New("not found in db")):
+			return nil, status.Error(codes.NotFound, "user not found")
+		default:
+			return nil, status.Error(codes.Internal, "internal error")
+		}
+
 	}
-	c.JSON(http.StatusOK, res)
+	return &proto_restaurant.RestaurantObject{
+		Id:      res.ID.String(),
+		Name:    res.Name,
+		Address: res.Address,
+		Contact: res.Contact,
+	}, nil
 }
 
-func (h *Handler) AddRestaurant(c *gin.Context) {
-	var tableInp models.RestaurantInputSql
-	if err := c.BindJSON(&tableInp); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		c.Abort()
-		return
+func (h *Handler) AddRestaurant(ctx context.Context, input *proto_restaurant.RestaurantObject) (*proto_restaurant.StatusResponse, error) {
+	if input.Id == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "id is required")
+	}
+	if input.Name == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "name is required")
+	}
+	if input.Address == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "address is required")
+	}
+	if input.Contact == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "contact is required")
 	}
 
-	table := models.RestaurantSql{Name: tableInp.Name,
-		Address: tableInp.Address, Contact: tableInp.Contact}
-
-	if err := h.service.Restaurants.Create(c.Request.Context(), &table); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"internal servar error": err.Error()})
-		c.Abort()
-		return
+	table := models.RestaurantSql{
+		Name:    input.GetName(),
+		Address: input.GetAddress(),
+		Contact: input.GetContact(),
+	}
+	if err := h.service.Restaurants.Create(ctx, &table); err != nil {
+		logger.Error(err)
+		switch {
+		default:
+			return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.Internal, "internal error")
+		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "restaurant made"})
+	return &proto_restaurant.StatusResponse{Status: true}, nil
 }
 
-func (h *Handler) DeleteRestaurantById(c *gin.Context) {
-	var delReserv models.DeleteInputSql
-	if err := c.BindJSON(&delReserv); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		c.Abort()
-		return
+func (h *Handler) DeleteRestaurantById(ctx context.Context, input *proto_restaurant.IDRequest) (*proto_restaurant.StatusResponse, error) {
+	if input.Id == "" {
+		return nil, status.Error(codes.InvalidArgument, "id is required")
 	}
 
-	err := h.service.Restaurants.DeleteById(c.Request.Context(), delReserv.DeleteId)
+	err := h.service.Restaurants.DeleteById(ctx, input.GetId())
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"bad reques": err.Error()})
-		c.Abort()
-		return
+		logger.Error(err)
+		switch {
+		default:
+			return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.Internal, "internal error")
+		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"Restaurant Deleted": nil})
+	return &proto_restaurant.StatusResponse{Status: true}, nil
 }
 
-func (h *Handler) UpdateRestById(c *gin.Context) {
-	var upRest models.UpdateRestaurantInputSql
-	if err := c.BindJSON(&upRest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		c.Abort()
-		return
+func (h *Handler) UpdateRestById(ctx context.Context, input *proto_restaurant.RestaurantObject) (*proto_restaurant.StatusResponse, error) {
+	if input.Id == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "id is required")
+	}
+	if input.Name == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "name is required")
+	}
+	if input.Address == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "address is required")
+	}
+	if input.Contact == "" {
+		return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.InvalidArgument, "contact is required")
 	}
 
-	err := h.service.Restaurants.UpdateById(c.Request.Context(), &upRest)
+	err := h.service.Restaurants.UpdateById(ctx, &models.UpdateRestaurantInputSql{
+		RestaurantId: input.GetId(),
+		Name:         input.GetName(),
+		Contact:      input.GetContact(),
+		Address:      input.GetAddress(),
+	})
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"bad reques": err.Error()})
-		c.Abort()
-		return
+		logger.Error(err)
+		switch {
+		default:
+			return &proto_restaurant.StatusResponse{Status: false}, status.Error(codes.Internal, "internal error")
+		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"updated restaurant": nil})
+	return &proto_restaurant.StatusResponse{Status: true}, nil
 }
