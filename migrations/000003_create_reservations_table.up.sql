114b36258d9509e2b47d7298d96f759239898ffd
aidostt <leket1999@gmail.com>
2024-04-27 06:08:45 +0500

    fixed migrations
    user table was dropped. Sequence of migrations is changed respectfully

--- migrations/000003_create_reservations_table.up.sql
+++ migrations/000003_create_reservations_table.up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS reservations (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    userid          TEXT NOT NULL REFERENCES users ON DELETE CASCADE,
+    userid          TEXT NOT NULL,
     tableid         UUID NOT NULL REFERENCES restables ON DELETE CASCADE,
     reservationtime TEXT
     );
